47c97fa28c5866a0047b871bc40066b567282d7b
Physick <96335032+DegustatorPonos@users.noreply.github.com>
2025-02-14 00:27:07 +0500

    Memory leaking fucked up quicksort

--- CommonLibs/ListTest.c
+++ CommonLibs/ListTest.c
@@ -4,21 +4,24 @@
 
 int main()
 {
-    int initDataLength = 8;
-    int initListData[] = {1, 2, 3 ,4, 1, 1, 2, 4};
+    int initDataLength = 9;
+    int initListData[] = {1, 9, 3 ,4, 1, 7, 9, 12, 11};
 
     LIST *list = ListInit();
     for(int i = 0; i < initDataLength; i++)
         AppendToList(list, initListData[i]);
     printf("Unsorted: ");
     PrintListInfo(list);
-    Bubblesort(list->array, list->Length);
+    QuickSort(list->array, list->Length);
     printf("Sorted: ");
     PrintListInfo(list);
-    RemoveAt(list, 1);
-    printf("Removed 2nd element: ");
-    PrintListInfo(list);
-    MapSortedList(list);
-    printf("Mapping: ");
-    PrintListInfo(list);
+    // RemoveAt(list, 1);
+    // printf("Removed 2nd element: ");
+    // PrintListInfo(list);
+    // MapSortedList(list);
+    // printf("Mapping: ");
+    // PrintListInfo(list);
+    ClearList(list);
+    free(list);
+    return 0;
 }
